2ad6fbfc159b0a970b9ff972f31ea8bf1156148f
galanisd <galanisd@gmail.com>
2017-05-17 16:58:49 +0300

    Updated.

--- omtd-cache-id/src/main/java/eu/openminted/omtdcache/CacheDataIDSHA1.java
+++ omtd-cache-id/src/main/java/eu/openminted/omtdcache/CacheDataIDSHA1.java
@@ -1,8 +1,5 @@
 package eu.openminted.omtdcache;
 
-import java.math.BigInteger;
-import java.security.MessageDigest;
-
 import org.apache.commons.codec.digest.DigestUtils;
 
 public class CacheDataIDSHA1 implements CacheDataID{
